5982eb3c95c9172ed4f0d3570316f6a1fb8bf5f5
hesamoon <he3am.jsm@mail.com>
2024-12-15 16:33:40 +0330

    add cors package

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import {
   getBills,
   getBill,
@@ -8,6 +9,7 @@ import {
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 function setCorsHeaders(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
